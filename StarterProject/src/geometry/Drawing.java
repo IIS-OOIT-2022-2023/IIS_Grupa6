a12cf9f993699c98308c7ddddebcb3ee5fb5fbd9
dejanagladic <dejanagladic@AR4-Asistent.ftn.ns>
2023-05-09 16:35:22 +0200

    Vezbe 8

--- StarterProject/src/geometry/Drawing.java
+++ StarterProject/src/geometry/Drawing.java
@@ -2,6 +2,8 @@ package geometry;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -14,26 +16,61 @@ public class Drawing extends JPanel {
 	public Drawing() {
 
 	}
-	
-	public static void main(String []args) {
+
+	public static void main(String[] args) {
 		JFrame frame = new JFrame("Drawing");
-		frame.setSize(800,600);
+		frame.setSize(800, 600);
 		Drawing drawing = new Drawing();
 		frame.getContentPane().add(drawing);
 		frame.setVisible(true);
 	}
+
 	public void paint(Graphics g) {
-		Point p1 = new Point(40,50);
-		p1.draw(g);
+		Point p = new Point(50, 50);
+		Line l1 = new Line(new Point(100, 100), new Point(200, 200));
+		Rectangle r1 = new Rectangle(l1.getEndPoint(), 100, 50);
+		Circle c1 = new Circle(new Point(50, 100), 80);
+		Donut d1 = new Donut(new Point(80, 100), 50, 25, true);
+		Rectangle k1 = new Rectangle(new Point(50, 50), 50, 50);
+
+		// Zadatak 1.
+		ArrayList<Shape> shapes = new ArrayList<Shape>();
+		shapes.add(p);
+		shapes.add(l1);
+		shapes.add(c1);
+		shapes.add(d1);
+		shapes.add(k1);
+		Iterator<Shape> it = shapes.iterator();
+		while (it.hasNext()) {
+			it.next().moveBy(10, 0);
+		}
+
+		try {
+			c1.setRadius(-15);
+			System.out.print("Da li se izvrsava?");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println("Izvrsava se");
+		}
 		
-		Line l1 = new Line(p1, new Point(70,80));
-		l1.draw(g);
+		// Zadatak 2.
+		shapes.get(3).draw(g);
+		shapes.get(shapes.size() - 1).draw(g);
+		shapes.remove(1);
+		// pomera se lista
+		shapes.get(1).draw(g);
+		shapes.get(3).draw(g);
+		shapes.add(3, l1);
 		
-		g.setColor(Color.RED);
-		Circle c1 = new Circle (new Point(100,100), 35);
-		c1.draw(g);
-		
+		it=shapes.iterator();
+		while(it.hasNext()) {
+			Shape pomocniS=it.next();
+			pomocniS.setSelected(true);
+			pomocniS.draw(g);
+		}
+
+
 	}
-	
 
 }
--- StarterProject/src/geometry/Rectangle.java
+++ StarterProject/src/geometry/Rectangle.java
@@ -1,13 +1,14 @@
 package geometry;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
-public class Rectangle extends Shape{
+public class Rectangle extends Shape {
 
 	private Point upperLeftPoint;
 	private int width;
 	private int height;
-	
+
 	public Rectangle() {
 	}
 
@@ -22,81 +23,91 @@ public class Rectangle extends Shape{
 		this(upperLeftPoint, width, height);
 		setSelected(selected);
 	}
-	
+
 	public boolean contains(int x, int y) {
-		return x >= upperLeftPoint.getX() && 
-				x <= upperLeftPoint.getX() + width && 
-				y >= upperLeftPoint.getY() &&
-				y <= upperLeftPoint.getY() + height;
+		return x >= upperLeftPoint.getX() && x <= upperLeftPoint.getX() + width && y >= upperLeftPoint.getY()
+				&& y <= upperLeftPoint.getY() + height;
 	}
-	
+
 	public int area() {
-		return width*height;
-	}  
-	
+		return width * height;
+	}
+
 	public int circumference() {
-		return 2*(width+height);
-	}  
-	
-	public boolean equals(Object obj) {		
-		if(obj instanceof Rectangle) {
-			Rectangle r = (Rectangle)obj;
-			if(this.upperLeftPoint.equals(r.upperLeftPoint)
-					&& this.width==r.width 
-					&& this.height==r.height) {
+		return 2 * (width + height);
+	}
+
+	public boolean equals(Object obj) {
+		if (obj instanceof Rectangle) {
+			Rectangle r = (Rectangle) obj;
+			if (this.upperLeftPoint.equals(r.upperLeftPoint) && this.width == r.width && this.height == r.height) {
 				return true;
 			}
-		}		
+		}
 		return false;
 	}
-	
+
 	public Point getUpperLeftPoint() {
 		return upperLeftPoint;
 	}
+
 	public void setUpperLeftPoint(Point upperLeftPoint) {
 		this.upperLeftPoint = upperLeftPoint;
 	}
+
 	public int getWidth() {
 		return width;
 	}
+
 	public void setWidth(int width) {
 		this.width = width;
 	}
+
 	public int getHeight() {
 		return height;
 	}
+
 	public void setHeight(int height) {
 		this.height = height;
 	}
-	
+
 	public String toString() {
 		return "Upper left point:" + upperLeftPoint + ", width =" + width + ",height = " + height;
 	}
 
 	@Override
 	public void draw(Graphics g) {
-		g.drawRect(upperLeftPoint.getX(), upperLeftPoint.getY(),width, height);
-		
+		g.drawRect(upperLeftPoint.getX(), upperLeftPoint.getY(), width, height);
+
+		if (isSelected()) {
+			g.setColor(Color.blue);
+			g.drawRect(upperLeftPoint.getX() - 2, upperLeftPoint.getY() - 2, 4, 4);
+			g.drawRect(upperLeftPoint.getX() + width - 2, upperLeftPoint.getY() - 2, 4, 4);
+			g.drawRect(upperLeftPoint.getX() - 2, upperLeftPoint.getY() + height - 2, 4, 4);
+			g.drawRect(upperLeftPoint.getX() + width - 2, upperLeftPoint.getY() 
+					+ height - 2, 4, 4);
+			g.setColor(Color.black);
+		}
+
 	}
 
 	@Override
 	public void moveTo(int x, int y) {
-		this.upperLeftPoint.moveTo(x, y);		
+		this.upperLeftPoint.moveTo(x, y);
 	}
 
 	@Override
 	public void moveBy(int byX, int byY) {
-		this.upperLeftPoint.moveBy(byX, byY);		
+		this.upperLeftPoint.moveBy(byX, byY);
 	}
-	
+
 	@Override
 	public int compareTo(Object obj) {
-		if(obj instanceof Rectangle) {
-			Rectangle shapeToCompare = (Rectangle)obj;
+		if (obj instanceof Rectangle) {
+			Rectangle shapeToCompare = (Rectangle) obj;
 			return this.area() - shapeToCompare.area();
 		}
 		return 0;
 	}
-	
-	
+
 }
